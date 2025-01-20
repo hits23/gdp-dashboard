739be3d4d8ebe74b5c7bc6510193a15bf8229fbe
hits23 <95261318+hits23@users.noreply.github.com>
2025-01-19 22:04:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,36 +5,55 @@ st.title('Hola')
 st.write('¡Hola, actualización 3!')
 
 
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import streamlit as st
 import pandas as pd
 import plotly.express as px
+import requests
+import zipfile
+import io
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+# URL del archivo ZIP en GitHub
+url_zip = "https://github.com/hits23/gdp-dashboard/raw/main/Cred_Dep_Of.zip"
 
-# Initialize the app
-app = Dash()
+# Descargar el archivo ZIP y mantenerlo en memoria
+response = requests.get(url_zip)
+archivo_zip_en_memoria = io.BytesIO(response.content)
 
-# App layout
-app.layout = [
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='my-final-radio-item-example'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='my-final-graph-example')
-]
+dataframes = []
 
-# Add controls to build the interaction
-@callback(
-    Output(component_id='my-final-graph-example', component_property='figure'),
-    Input(component_id='my-final-radio-item-example', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
+# Leer el archivo ZIP desde la memoria
+with zipfile.ZipFile(archivo_zip_en_memoria, 'r') as archivo_zip:
+    nombres_archivos = archivo_zip.namelist()
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+    for nombre_archivo in nombres_archivos:
+        with archivo_zip.open(nombre_archivo) as archivo:
+            contenido = archivo.read().decode('utf-8')
+
+            try:
+                df = pd.read_csv(io.StringIO(contenido), delimiter=',', encoding='utf-8')
+                df['nombre_archivo'] = nombre_archivo  # Agregar una columna con el nombre del archivo
+                dataframes.append(df)
+            except Exception as e:
+                st.write(f'Error al leer {nombre_archivo}: {e}')
+
+# Concatenar todos los DataFrames
+df = pd.concat(dataframes, ignore_index=True)
+
+# Título de la aplicación
+st.title('Mi Primera Aplicación con Datos, Gráficas y Controles')
+
+# Controles de selección de columna
+col_chosen = st.radio('Selecciona una columna:', ['pop', 'lifeExp', 'gdpPercap'], index=1)
+
+# Mostrar la tabla de datos
+st.dataframe(df)
+
+# Crear una gráfica interactiva
+fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+
+# Mostrar la gráfica
+st.plotly_chart(fig)
+
+
+streamlit run app.py
 
